193998066db8b3037b382631a6cd50d83464fffe
Sonu Kumar <sonu.kumar@NBC02ZG2DJLVCG.EMIRATES.COM>
2020-09-07 18:14:11 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+function App() {
+  const counter = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <button
+        onClick={() =>
+          dispatch({
+            type: "Car",
+          })
+        }
+      >
+        Car
+      </button>{" "}
+      &nbsp;&nbsp;&nbsp;
+      <h1>{counter.vehicle}</h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "Bike",
+          })
+        }
+      >
+        Bike
+      </button>
+    </div>
+  );
+}
+
+export default App;
